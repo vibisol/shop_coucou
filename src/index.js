b912204dea695b9b50ada77cdffc2704ffeaa3d5
Victor Khatuncev <139468165+vibisol@users.noreply.github.com>
2024-11-09 13:11:17 +0300

    refactor: add error boundary

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
+import ErrorBoundary from './errorBoundary';
+
+window.onerror = function(message, source, lineno, colno, error) {
+  console.error("Global error handler:", message, source, lineno, colno, error);
+  return true; // Предотвращает отображение ошибки пользователю
+};
+
+window.addEventListener('unhandledrejection', function(event) {
+  console.error("Unhandled promise rejection:", event.reason);
+  event.preventDefault(); // Предотвращает отображение ошибки пользователю
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 );
